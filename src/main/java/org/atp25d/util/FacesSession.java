c42647966bfc0ed68324875641349cd4341617ec
Rusbourne, James <James.Rusbourne@det.nsw.edu.au>
2016-09-16 12:51:44 +1000

    remove theme

--- src/main/java/org/atp25d/util/FacesSession.java
+++ src/main/java/org/atp25d/util/FacesSession.java
@@ -31,8 +31,6 @@ import javax.inject.Named;
 		@Inject
 	    private FacesContext facesContext;	
 		
-	    private Map<String, String> themes;
-	    private String theme;  
 		
 //		   public BaseUser getLoggedInUser(){
 //			   BaseUser user = (BaseUser)facesContext.getExternalContext().getSessionMap().get("ESSUser");    		
@@ -59,58 +57,10 @@ import javax.inject.Named;
 		       }	   
 		     return nodename;
 		   }
-		   public String getTheme() {
-			   if(theme == null || theme.length() == 0) theme = "cupertino";
-		       return theme;
-		   }
 
-		   public void setTheme(String theme) {
-		       this.theme = theme;
-		   }
 		   
-		   public Map<String, String> getThemes() {
-		       return themes;
-		   }
 			@PostConstruct	
 			   public void initMe() {	    	
-		   	 	theme = "cupertino";	   	 	
-		        themes = new TreeMap<>();
-		        themes.put("Afterdark", "afterdark");
-		        themes.put("Afternoon", "afternoon");
-		        themes.put("Afterwork", "afterwork");
-		        themes.put("Aristo", "aristo");
-		        themes.put("Black-Tie", "black-tie");
-		        themes.put("Blitzer", "blitzer");
-		        themes.put("Bluesky", "bluesky");
-		        themes.put("Bootstrap", "bootstrap");
-		        themes.put("Casablanca", "casablanca");
-		        themes.put("Cupertino", "cupertino");
-		        themes.put("Cruze", "cruze");
-		        themes.put("Dark-Hive", "dark-hive");
-		        themes.put("Dot-Luv", "dot-luv");
-		        themes.put("Eggplant", "eggplant");
-		        themes.put("Excite-Bike", "excite-bike");
-		        themes.put("Flick", "flick");
-		        themes.put("Glass-X", "glass-x");
-		        themes.put("Hot-Sneaks", "hot-sneaks");
-		        themes.put("Home", "home");
-		        themes.put("Humanity", "humanity");
-		        themes.put("Le-Frog", "le-frog");
-		        themes.put("Midnight", "midnight");
-		        themes.put("Mint-Choc", "mint-choc");
-		        themes.put("Overcast", "overcast");
-		        themes.put("Pepper-Grinder", "pepper-grinder");
-		        themes.put("Redmond", "redmond");
-		        themes.put("Rocket", "rocket");
-		        themes.put("Sam", "sam");
-		        themes.put("Smoothness", "smoothness");
-		        themes.put("South-Street", "south-street");
-		        themes.put("Start", "start");
-		        themes.put("Sunny", "sunny");
-		        themes.put("Swanky-Purse", "swanky-purse");
-		        themes.put("Trontastic", "trontastic");
-		        themes.put("UI-Darkness", "ui-darkness");
-		        themes.put("UI-Lightness", "ui-lightness");
 			   }	   
 	}
 
