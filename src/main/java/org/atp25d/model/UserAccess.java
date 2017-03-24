b075a1cd14de8d2b27fa9118ac306e497e048e52
Rusbourne, James <James.Rusbourne@det.nsw.edu.au>
2017-03-24 16:33:19 +1100

    add new table for user access

---
+++ src/main/java/org/atp25d/model/UserAccess.java
@@ -0,0 +1,63 @@
+package org.atp25d.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the user_access database table.
+ * 
+ */
+@Entity
+@Table(name="user_access")
+@NamedQuery(name="UserAccess.findAll", query="SELECT u FROM UserAccess u")
+public class UserAccess implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="iduser_access")
+	private int iduserAccess;
+
+	private String userAccess;
+
+	private String userId;
+
+	private String userTask;
+
+	public UserAccess() {
+	}
+
+	public int getIduserAccess() {
+		return this.iduserAccess;
+	}
+
+	public void setIduserAccess(int iduserAccess) {
+		this.iduserAccess = iduserAccess;
+	}
+
+	public String getUserAccess() {
+		return this.userAccess;
+	}
+
+	public void setUserAccess(String userAccess) {
+		this.userAccess = userAccess;
+	}
+
+	public String getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserTask() {
+		return this.userTask;
+	}
+
+	public void setUserTask(String userTask) {
+		this.userTask = userTask;
+	}
+
+}
